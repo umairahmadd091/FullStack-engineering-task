3ee331ef04eaaf3c54dd481897f8a9e3c30b8e31
Umair Ahmad <47920569+umairahmadd091@users.noreply.github.com>
2022-02-14 14:30:02 +0500

    Task
    There are total 3 tasks in the project.
    task 1: http://localhost:3000/task1/i/want/title?address=dawn.com/   (Done with plain nodejs and express)
    task 2: http://localhost:3000/task2/i/want/title?address=dawn.com/   (Done with async.js library)
    task 3: http://localhost:3000/task3/i/want/title?address=google.com/   (Done with RSVP library)

---
+++ app.js
@@ -0,0 +1,87 @@
+import fetch from "node-fetch";
+import express from "express";
+const app = express();
+import async from "async";
+import RSVP from "rsvp";
+
+const parseTitle = (body) => {
+  let match = body.match(/<title>([^<]*)<\/title>/);
+  if (!match || typeof match[1] !== "string")
+    throw new Error("Unable to parse the title tag");
+  return match[1];
+};
+
+
+//////////////////////////////////////////
+
+app.get("/task1/i/want/title", (req, res) => {
+  const { address } = req.query;
+  if (!address) return res.status(404).end("Missing url query parameter");
+  const newAdress = address.substring(0, 11);
+
+  fetch(newAdress !== "https://www" ? "https://www." + address : address)
+    .then((res) => res.text())
+    .then((body) => parseTitle(body))
+    .then((title) =>{
+     const query = `<h1>List of Websites:</h1> <ul><li> ${address} - ${title}</li></ul>`;
+      res.send(query)}
+    )
+    .catch((e) => res.status(404).send(`<li> ${address} - No Response </li>`));
+});
+
+
+// ////////////////////////////////////////////////////
+
+app.get("/task2/i/want/title", (req, res) => {
+  const { address } = req.query;
+  if (!address) return res.status(404).end("Missing url query parameter");
+  const newAdress = address.substring(0, 11);
+
+  async
+    .parallel([
+      async () => {
+        const data = await fetch(
+          newAdress !== "https://www" ? "https://www." + address : address
+        );
+        const textData = await data.text();
+        const finalData = await parseTitle(textData);
+        return finalData;
+      },
+    ])
+    .then((title) => {
+      const query = `<h1>List of Websites:</h1> <ul><li> ${address} - ${title}</li></ul>`;
+      res.send(query);
+    })
+    .catch((e) => res.status(404).send(`<li> ${address} - No Response </li>`));
+});
+
+// ////////////////////////////////////////////
+
+app.get("/task3/i/want/title", (req, res) => {
+  const { address } = req.query;
+  if (!address) return res.status(404).end("Missing url query parameter");
+
+  const newAdress = address.substring(0, 11);
+
+  let promise = new RSVP.Promise((resolve, reject) => {
+    fetch(newAdress !== "https://www" ? `https://www.${address}` : address)
+      .then((res) => res.text())
+      .then((body) => parseTitle(body))
+      .then((title) => resolve(title))
+      .catch((e) => reject(e));
+  });
+
+  promise
+    .then((title) => {
+      const query = `<h1>List of Websites:</h1> <ul><li> ${address} - ${title}</li></ul>`;
+
+      res.send(query);
+    })
+    .catch((error) => {
+      res.status(404).send(`<li> ${address} - No Response </li>`);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("server running on 3000");
+});
