167e021ec8daad725847d0a9a3b775845bb195c9
Umair Ahmad <47920569+umairahmadd091@users.noreply.github.com>
2022-02-16 17:28:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 import async from "async";
 import RSVP from "rsvp";
+import https from "https";
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const parseTitle = (body) => {
   let match = body.match(/<title>([^<]*)<\/title>/);
@@ -19,14 +26,23 @@ app.get("/task1/i/want/title", (req, res) => {
   if (!address) return res.status(404).end("Missing url query parameter");
   const newAdress = address.substring(0, 11);
 
-  fetch(newAdress !== "https://www" ? "https://www." + address : address)
-    .then((res) => res.text())
-    .then((body) => parseTitle(body))
-    .then((title) =>{
-     const query = `<h1>List of Websites:</h1> <ul><li> ${address} - ${title}</li></ul>`;
-      res.send(query)}
-    )
-    .catch((e) => res.status(404).send(`<li> ${address} - No Response </li>`));
+  const request = https.request(
+    newAdress !== "https://www" ? "https://www." + address : address,
+    (res) => {
+      res.on("data", (d) => {
+        let title = parseTitle(d.toString());
+        process.stdout.write(
+          `<h1>List of Websites:</h1> <ul><li> ${address} - ${title}</li></ul>`
+        );
+      });
+    }
+  );
+  request.on("error", (error) => {
+    console.error(error);
+  });
+
+  request.end();
+
 });
 
 
@@ -44,8 +60,7 @@ app.get("/task2/i/want/title", (req, res) => {
           newAdress !== "https://www" ? "https://www." + address : address
         );
         const textData = await data.text();
-        const finalData = await parseTitle(textData);
-        return finalData;
+        return parseTitle(textData);
       },
     ])
     .then((title) => {
